240a6d3ee005074ed44c8d5b2e197eaaedc1d777
domingo79 <andria140779@yahoo.it>
2021-05-13 20:14:46 +0200

    milestone-3

--- asset/js/main.js
+++ asset/js/main.js
@@ -21,22 +21,65 @@ const icone = [
 ];
 
 // Milestone 2 Coloriamo le icone per tipo
+const animalIcone = [], vegetableIcone = [], userIcone = [];
+
 icone.forEach((icona) => {
     if (icona.type === 'animal') {
         icona.color = 'blue';
+        animalIcone.push(icona)
     } else if (icona.type === 'vegetable') {
         icona.color = 'orange';
+        vegetableIcone.push(icona)
     } else if (icona.type === 'user') {
         icona.color = 'red';
+        userIcone.push(icona)
     }
-    document.getElementById('test').insertAdjacentHTML('beforeend', `
-    <div>
-    <i class="${icona.family} ${icona.prefix}${icona.name}" style="color:${icona.color}" ></i> 
-    <h2>${icona.name} </h2>
-    </div>
-    `)
+    // document.getElementById('markUp').insertAdjacentHTML('beforeend', `
+    //     <div>
+    //     <i class="${icona.family} ${icona.prefix}${icona.name}" style="color:${icona.color}" ></i> 
+    //     <h2>${icona.name} </h2>
+    //     </div>
+    //     `)
+
 });
 
 // Milestone 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 
-// console.log(icone);
+const selezione = document.getElementById('filter');
+selezione.addEventListener('change', () => {
+
+    const valore = selezione.value;
+    //console.log(valore);
+
+    let markUp = []
+
+    if (valore === 'all') {
+        markUp = icone;
+        console.log(markUp);
+    } else if (valore === 'animal') {
+        markUp = animalIcone;
+        console.log(markUp);
+    } else if (valore === 'vegetable') {
+        markUp = vegetableIcone;
+        console.log(markUp);
+    } else if (valore === 'user') {
+        markUp = userIcone;
+        console.log(markUp);
+    }
+
+    markUp.forEach((elem) => {
+
+        const { name, family, prefix, color } = elem;
+
+        document.getElementById('markUp').insertAdjacentHTML('beforeend', `
+        <div>
+        <i class="${family} ${prefix}${name}" style="color:${color}" ></i> 
+        <h2>${name} </h2>
+        </div>
+        `)
+    })
+
+})
+
+
+
